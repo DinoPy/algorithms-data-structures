ec82664926c81e58c0ed94c25ef0412a154787f0
DinoPy <al3x.dinoiu@gmail.com>
2024-10-23 19:26:09 +0300

    made bubble sort, same version in js, py and c++

---
+++ c++/bubble_sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> test_list = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+std::vector<int> bubble_sort_1(std::vector<int> list) {
+    bool swapping = true;
+    size_t end = list.size();
+    while (swapping) {
+        swapping = false;
+        for (int i = 1; i < end; ++i) {
+            if (list[i - 1] > list[i]) {
+                auto temp = list[i];
+                list[i] = list[i - 1];
+                list[i - 1] = temp;
+                swapping = true;
+            }
+        }
+        end -= 1;
+    }
+    return list;
+}
+
+void print(std::vector<int> list) {
+    for (auto &i : list) {
+        std::cout << i;
+    }
+    std::cout << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    std::cout << "Original list: ";
+    print(test_list);
+    std::cout << "\nOrdered list: ";
+    print(bubble_sort_1(test_list));
+    std::cout << std::endl;
+    return 0;
+}
